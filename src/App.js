caa5ab5690a412aefe1461abada9298af9387ea1
Jasdeepkaur1381 <kjasdeep219@gmail.com>
2023-06-27 15:36:42 +0530

    redirect feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from "./ReactMenu/Home";
 import About from "./ReactMenu/About";
 import Service from "./ReactMenu/Services";
 import Contact from "./ReactMenu/Contact";
 import Navbar from "./ReactMenu/Navbar";
 import Footer from "./ReactMenu/Footer";
+import { createBrowserHistory } from 'history';
 import './App.css';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const App = () => {
+
   const navigate = useNavigate();
+  const history = createBrowserHistory();
+
+  useEffect(() => {
+    const redirectTimeout = setTimeout(() => {
+      navigate('/');
+    }, 1000);
+
+    return () => clearTimeout(redirectTimeout); // Clean up the timeout on unmount
+  }, [navigate]);
   return (
     <>
       <Navbar />
